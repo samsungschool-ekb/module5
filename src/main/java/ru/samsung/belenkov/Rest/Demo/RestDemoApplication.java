8bc9235f819dc261f29ce98a67a1b5a02b9d449b
Vlad Belekov <v.belenkov@git.socium.local>
2019-04-15 11:01:07 +0500

    add simple rest api example

---
+++ src/main/java/ru/samsung/belenkov/Rest/Demo/RestDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.samsung.belenkov.Rest.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemoApplication.class, args);
+	}
+
+}
